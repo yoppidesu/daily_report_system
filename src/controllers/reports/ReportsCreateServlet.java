436c8fd47c91ade86338f3344f457517a98d4e46
techyoppi <techyoppi@docomo.ne.jp>
2020-10-27 19:43:29 +0900

    Add Report show

--- src/controllers/reports/ReportsCreateServlet.java
+++ src/controllers/reports/ReportsCreateServlet.java
@@ -21,13 +21,13 @@ import utils.DBUtil;
  * Servlet implementation class ReportCreateServlet
  */
 @WebServlet("/reports/create")
-public class ReportCreateServlet extends HttpServlet {
+public class ReportsCreateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReportCreateServlet() {
+    public ReportsCreateServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
